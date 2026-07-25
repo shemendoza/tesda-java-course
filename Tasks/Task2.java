323e2dfa6513ac12be2b6016cc5adf8fd553c8da
shemendoza <110075797+shemendoza@users.noreply.github.com>
2026-07-25 09:35:38 +0000

    Tasks/Task2.java

---
+++ Tasks/Task2.java
@@ -0,0 +1,16 @@
+package Tasks;
+
+public class Task2{
+public static void main(String[] args) {
+    short three = 3;
+    int one = 1;
+    byte zero = 0;
+    char r = 'r';
+    float two_point_zero = 2.0f;
+    boolean isTrue = true;
+
+        String output = "H" + three + one + one + zero + " w" + zero +"r" + one + "d " + two_point_zero + " " + isTrue;
+        System.out.println(output); 
+    }
+
+}
