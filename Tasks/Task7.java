8ea28ebfb7353f6955f616099bdb2f6298da0be5
shemendoza <110075797+shemendoza@users.noreply.github.com>
2026-08-06 08:31:07 +0000

    Tasks/Task7.java

---
+++ Tasks/Task7.java
@@ -0,0 +1,38 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task7 {
+
+     public static double Addition(double a, double b){
+            return a+b;
+        }
+
+        public static double Subtraction(double a, double b){
+            return a-b;
+        }
+        public static double Multiplication(double a, double b){
+            return a*b;
+        }
+        public static double Division(double a, double b){
+            return a/b;
+        }
+    public static void main(String[] args) {
+       Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = userInput.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = userInput.nextDouble();
+
+        System.out.println("Addition: " + Addition(num1, num2));
+        System.out.println("Subtraction: " + Subtraction(num1, num2));
+        System.out.println("Multiplication: " + Multiplication(num1, num2));
+
+        if (num2 != 0)
+        System.out.println("Division: " + Division(num1, num2));
+    else
+        System.out.println("Division: Cannot divide by zero");
+    }
+
+    
+}
