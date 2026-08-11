30fb57cacd422e770610c9eec0f5d2c3df182422
shemendoza <110075797+shemendoza@users.noreply.github.com>
2026-08-11 11:41:26 +0800

    Created the user model

--- jcash/src/main/java/com/jcash/Main.java
+++ jcash/src/main/java/com/jcash/Main.java
@@ -1,8 +1,22 @@
 package com.jcash;
 
+import com.jcash.model.User;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello JCash");
+        User user = new User("09123456789", "1234", "She Mendoza");
+
+        user.deposit(20000);
+        user.withdraw(5000);
+
+        System.out.println("Name: " + user.getFullName());
+        System.out.println("Mobile Number: " + user.getMobileNumber());
+        System.out.println("Balance: " + user.getBalance());
+
+        System.out.println("Transactions:");
+        for(String transaction : user.getTransactions()){
+            System.out.println(transaction);
+        }
     }
 }
--- jcash/src/main/java/com/jcash/model/User.java
+++ jcash/src/main/java/com/jcash/model/User.java
@@ -1,4 +1,72 @@
 package com.jcash.model;
 
+import java.util.*;
+
 public class User {
+    private String mobileNumber;
+    private String pinNumber;
+    private String fullName;
+    private double balance;
+    private List<String> transactions;
+
+    public User (
+     String mobileNumber,
+     String pinNumber,
+     String fullName){
+        this.mobileNumber = mobileNumber;
+        this.pinNumber = pinNumber;
+        this.fullName = fullName;
+        this.balance = 0.0;
+        this.transactions = new ArrayList<String>();
+    }
+    public void setMobileNumber(String mobileNumber){
+        this.mobileNumber = mobileNumber;
+    }
+
+    public void setPinNumber(String pinNumber){
+        this.pinNumber = pinNumber;
+    }
+
+    public void setFullName(String fullName){
+        this.fullName = fullName;
+    }
+
+    public void deposit(double amount){
+        if (amount > 0){
+            balance += amount;
+            transactions.add("Deposit: " + amount);
+        }
+    }
+
+    public void withdraw(double amount){
+        if (amount > 0 && amount <= balance){
+            balance -= amount;
+            transactions.add("Withdrawal: " + amount);
+        }
+
+    }
+
+    public void setTransactions(String transactions) {
+        this.transactions.add(transactions);
+    }
+
+    public String getMobileNumber(){
+        return mobileNumber;
+    }
+
+    public String getPinNumber(){
+        return pinNumber;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<String> getTransactions() {
+        return transactions;
+    }
 }
