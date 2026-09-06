5f824ddcde9740e09c021e732c76efea11c316ed
shemendoza <110075797+shemendoza@users.noreply.github.com>
2026-09-06 05:41:28 +0000

    Task Sheet 1-1-3

---
+++ task_sheets/tasksheet1_1_3.java
@@ -0,0 +1,16 @@
+package task_sheets;
+
+public class tasksheet1_1_3 {
+    
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message;
+
+        for(int i = 1; i <= check_number; i++ )
+        {
+            message = (i % 2 == 0) ? i + " is even number" : i + " is odd number";
+            System.out.println(message);
+        }
+    }
+    
+}
