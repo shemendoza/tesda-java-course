932c60aa53fe2ce4c01834390d38bac354ce2112
shemendoza <110075797+shemendoza@users.noreply.github.com>
2026-09-06 05:32:34 +0000

    Task Sheet 1-1-2

---
+++ task_sheets/tasksheet1_1_2.java
@@ -0,0 +1,25 @@
+package task_sheets;
+
+public class tasksheet1_1_2{
+    public static void main (String[] args){
+          
+        int result = 1;
+
+        result += 2;
+        System.out.println(result);
+
+        result -= 1;
+        System.out.println(result);
+
+        result *= 2;
+        System.out.println(result);
+
+        result /= 2;
+        System.out.println(result);
+
+        result += 8;
+        result %= 7;
+        System.out.println(result);
+
+    }
+}
